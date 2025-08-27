c2c83d2b56a4aadc3b003e3ed16272ecec2bb468
KSENOU <kalifasenou10@gmail.com>
2025-08-27 07:58:39 +0000

    update urls file dans le fichier principal

--- denni_ba_proj/urls.py
+++ denni_ba_proj/urls.py
@@ -19,4 +19,9 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #url pour pointer sur l'app denniba
+    path('api/', include('denniba.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
